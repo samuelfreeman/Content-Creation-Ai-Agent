be14ae7b2e6ac4972d13f504246fc2f8bd268d49
samuelfreeman <samuelmanueljnr@gmail.com>
2025-10-25 02:52:37 +0000

    changed to cloud model

--- test.js
+++ test.js
@@ -1,13 +1,19 @@
-import ollama from "ollama";
+import { Ollama } from "ollama";
+import { configDotenv } from "dotenv";
+configDotenv();
+const ollama = new Ollama({});
 
 const gettingStarted = async () => {
   try {
     const response = await ollama.chat({
-      model: "gemma3",
-      messages: [{ role: "user", content: "Hello, Ollama!" }],
+      model: "gpt-oss:120b-cloud",
+      messages: [{ role: "user", content: "Hello Ollama" }],
+      stream: true,
     });
 
-    console.log(response.message.content);
+    for await (const part of response) {
+      process.stdout.write(part.message.content);
+    }
   } catch (error) {
     console.log("Error:", error);
   }
