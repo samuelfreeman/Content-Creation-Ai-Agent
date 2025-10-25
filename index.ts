6c0d66db4818fa01e324049f6dc73184915823e3
samuelfreeman <samuelmanueljnr@gmail.com>
2025-10-25 12:58:30 +0000

    creating an mcp server in ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+// import mcpserver to provide apis for working with tools and resourceTemplate for url matching
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+
+import express from 'express'
+
+import zod  from 'zod'
