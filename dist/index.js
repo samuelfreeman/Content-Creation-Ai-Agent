7bc24d7dd1f2512be268b4572702867b59149b8f
samuelfreeman <samuelmanueljnr@gmail.com>
2025-10-25 13:51:41 +0000

    setting up a simple server

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,28 @@
-// import mcpserver to provide apis for working with tools and resourceTemplate for url matching
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
-// import stream transport for sending streamable http responses
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-// import express for creating the node server 
-import express from 'express'
-// import zod for schema validation
-import zod from 'zod'
-
+import express from 'express';
 const server = new McpServer({
     name: "Content creation",
     version: "1.0.0"
-})
-
-
+});
 const app = express();
 app.use(express.json());
-
-
-
 app.post('/mcp', async (req, res) => {
-
     const transport = new StreamableHTTPServerTransport({
         sessionIdGenerator: undefined,
         enableJsonResponse: true
-    })
+    });
     res.on('close', () => {
         transport.close();
-    })
-
+    });
     await server.connect(transport);
     await transport.handleRequest(req, res, req.body);
-})
-
-const port = 3000
+});
+const port = 3000;
 app.listen(port, () => {
     console.log(`Demo Mcp server  running on http://locahost:${port}/mcp`);
-
 }).on('error', (error) => {
-    console.error("Server error:", error)
-    process.exit(1)
-})
+    console.error("Server error:", error);
+    process.exit(1);
+});
+//# sourceMappingURL=index.js.map
